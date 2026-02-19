66cd6389afac6d9f87d7cb24371226f8563928e5
Diana Fernanda Belle <garbados@gmail.com>
2026-02-18 23:37:51 -0800

    that makes more sense

--- src/my_blog/hbs.clj
+++ src/my_blog/hbs.clj
@@ -1,4 +1,4 @@
 (ns my-blog.hbs
   (:require [hbs.core :as hbs]))
 
-(def reg (hbs/registry (hbs/classpath-loader "resources/hbs" ".hbs")))
+(def reg (hbs/registry (hbs/file-loader "resources/hbs" ".hbs")))
